e3f9cf13c7c2426c261f44bda836d3e4c1959f05
YEJI <kimyeji315@gmail.com>
2023-10-24 21:22:52 +0900

    231024

---
+++ BaekjoonOnlineJudge/src/main/java/algorythm/month10/yeji/Exam1008.java
@@ -0,0 +1,16 @@
+package algorythm.month10.yeji;
+
+import java.util.Scanner;
+
+public class Exam1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        System.out.println(a/b);
+    }
+}
+/*
+* https://imototot.tistory.com/186
+* https://codingdog.tistory.com/entry/백준에서-자주-보이는-절대-오차-상대-오차에-대해-간단하게-알아봅시다
+* */
